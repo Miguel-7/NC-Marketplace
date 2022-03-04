db4a2e7d2990936996630909d62d17a752df4b02
Damog83 <damiengittings@hotmail.co.uk>
2022-03-04 21:13:01 +0000

    form displaying

--- src/components/SellItemPage.jsx
+++ src/components/SellItemPage.jsx
@@ -4,25 +4,32 @@ return (
     <div>
         <h2>Sell Item</h2>
         <form>
-            <label for="name">Name:</label><br>
-            <input type="text" id="name" name="name"><br>
-            <label for="image">Picture:</label><br>
-            <input type="image" id="image" name="image"><br>
-            <label for="price">Price:</label><br>
-            <input type="number" id="price" name="price"><br>
-            <label for="description">Description:</label><br>
-            <input type="text" id="description" name="description"><br>
+            <label for="name"> Name </label>
+
+            <input type="text" id="name" name="name"/><br/>
             
-            <label for="category">Select category:</label>
+            <label for="image"> Picture </label>
+
+            <input type="url" id="image" name="image"/><br/>
+            
+            <label for="price">  Price  </label>
+
+            <input type="number" id="price" name="price"/><br/>
+            
+            <label for="description">  Description  </label>
+
+            <input type="text" id="description" name="description"/><br/>
+            
+            <label for="category"> Select category </label>
             
             <select name="category" id="category">
-                <option value="electronics">Electronics</option>
-                <option value="relics">Relics</option>
-                <option value="household">Household</option>
+                <option value="electronics"> Electronics </option>
+                <option value="relics"> Relics </option>
+                <option value="household"> Household </option>
             </select>
         </form>
     </div>
-)
+);
 }
 
 export default SellItemPage;
